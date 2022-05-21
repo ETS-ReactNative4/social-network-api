fae1fdce54322b7e90286577954971efae764c4f
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-05-21 18:22:26 -0400

    Updated Schema for user to include username, email, and validation

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+// const assignmentSchema = require('./Assignment');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    github: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    // assignments: [assignmentSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
