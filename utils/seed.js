156181c8895f29e51eb4ad2001ccb46ee72d5288
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-05-21 22:18:15 -0400

    Added starter seed functionality

--- utils/seed.js
+++ utils/seed.js
@@ -1,51 +1,28 @@
 const connection = require('../config/connection');
-const { Course, Student } = require('../models');
-const { getRandomName, getRandomAssignments } = require('./data');
+const { User, Thought } = require('../models');
+const userData = require('./userData');
+const thoughtData = require('./thoughtData');
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
   console.log('connected');
 
-  // Drop existing courses
-  await Course.deleteMany({});
+  // Drop existing data
+  await User.deleteMany({});
+  await Thought.deleteMany({});
 
-  // Drop existing students
-  await Student.deleteMany({});
+  const users = userData;
+  const thoughts = thoughtData;
 
-  // Create empty array to hold the students
-  const students = [];
+  // Add data to collection
+  await User.collection.insertMany(users);
+  await Thought.collection.insertMany(thoughts);
 
-  // Loop 20 times -- add students to the students array
-  for (let i = 0; i < 20; i++) {
-    // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
-
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
-    students.push({
-      first,
-      last,
-      github,
-      assignments,
-    });
-  }
-
-  // Add students to the collection and await the results
-  await Student.collection.insertMany(students);
-
-  // Add courses to the collection and await the results
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
-    inPerson: false,
-    students: [...students],
-  });
 
   // Log out the seed data to indicate what should appear in the database
-  console.table(students);
+  console.table(users);
+  console.table(thoughts);
   console.info('Seeding complete! 🌱');
   process.exit(0);
 });
